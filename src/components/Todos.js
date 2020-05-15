3d2123e8cf86c46b3e0aa8f56aebe25e6a1f5c5d
Oskar Hellkvist <oh222gw@student.lnu.se>
2020-05-15 12:14:00 +0200

    tidying

--- src/components/Todos.js
+++ src/components/Todos.js
@@ -3,8 +3,6 @@ import React from 'react'
 import TodoItem from './TodoItem'
 
 class Todos extends React.Component {
-    
-
     render () {
         if (this.props.todos !== undefined) {
             return (
